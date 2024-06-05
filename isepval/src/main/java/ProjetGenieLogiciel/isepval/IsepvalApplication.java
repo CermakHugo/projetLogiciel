e16ea0c86370f28e4ae7728cedeb64708e866074
Cermak <115461395+CermakHugo@users.noreply.github.com>
2024-06-05 10:00:58 +0200

    Add files via upload

---
+++ isepval/src/main/java/ProjetGenieLogiciel/isepval/IsepvalApplication.java
@@ -0,0 +1,13 @@
+package ProjetGenieLogiciel.isepval;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsepvalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsepvalApplication.class, args);
+	}
+
+}
---
+++ isepval/src/test/java/ProjetGenieLogiciel/isepval/IsepvalApplicationTests.java
@@ -0,0 +1,13 @@
+package ProjetGenieLogiciel.isepval;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsepvalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
